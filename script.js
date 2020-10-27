0b20e01caf7d6b7ee81ebab297c4a8f95ec9b0ff
sahan94 <34939557+sahan94@users.noreply.github.com>
2020-10-27 22:22:15 +0530

    v 1

---
+++ script.js
@@ -0,0 +1,20 @@
+//browser-sync start --proxy "myproject.dev" --files "*/**"
+
+$("#submit").click(function(e){
+	e.preventDefault()
+	var ID = {
+		name : $("#name").val(),
+		address : $("#address").val(),
+		phoneNo : $("#phone").val(),
+		id : $("#name").val()
+	}
+	
+	var cnvrtStrinJason = JSON.stringify(ID)
+	console.log(cnvrtStrinJason)
+	
+	var url = $("#form").attr("action")
+
+	$.post(url,ID,function(){
+		alert("sumited")
+	})
+})
